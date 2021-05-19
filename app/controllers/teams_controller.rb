458b325e32e4e5c165651babf32b1b23db007753
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-19 17:46:59 +0900

    update:チームリーダーが、他のチームメンバーにリーダー権限を渡せる機能を実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,6 +9,9 @@ class TeamsController < ApplicationController
   def show
     @working_team = @team
     change_keep_team(current_user, @team)
+    if params[:selected_user].present?
+      change_team_admin(params[:selected_user],@working_team)
+    end
   end
 
   def new
@@ -46,7 +49,7 @@ class TeamsController < ApplicationController
   def dashboard
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
   end
-
+  
   private
 
   def set_team
